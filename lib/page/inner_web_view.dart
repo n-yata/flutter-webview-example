0ef0c53d45cf2c1e1fee5057377b60b88e388501
nyata <yata1214@gmail.com>
2023-10-16 06:30:00 +0900

    inner webviewの微修正

--- lib/page/inner_web_view.dart
+++ lib/page/inner_web_view.dart
@@ -6,14 +6,37 @@ class InnerWebView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("webView"),
+    return WillPopScope(
+        onWillPop: () async => false,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(50),
+            child: AppBar(
+              // title: const Text("webView"),
+              backgroundColor: Colors.white,
+              automaticallyImplyLeading: false,
+              leading: backButton(context),
+            ),
+          ),
+          body: const WebView(
+            initialUrl: 'https://github.com/n-yata',
+            javascriptMode: JavascriptMode.unrestricted,
+          ),
+        ));
+  }
+
+  /// 戻るボタン
+  TextButton backButton(BuildContext context) {
+    return TextButton(
+      child: const Text(
+        '×',
+        style: TextStyle(
+          color: Colors.black, //文字の色を白にする
+          fontWeight: FontWeight.normal, //文字を太字する
+          fontSize: 30.0, //文字のサイズを調整する
+        ),
       ),
-      body: const WebView(
-        initialUrl: 'https://github.com/n-yata',
-        javascriptMode: JavascriptMode.unrestricted,
-      ),
+      onPressed: () => Navigator.of(context).pop(),
     );
   }
 }
